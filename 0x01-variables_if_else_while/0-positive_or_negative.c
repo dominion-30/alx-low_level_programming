a2308238eba4aaeb229c33b9f1d5e7a22d3964a7
dominion-30 <abiolaayobamidele@gmail.com>
2022-03-11 12:39:59 -0800

    modify file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,9 +4,15 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+
+/**
+ * 0-positive_or_negative-if the number is greater than 0: is positive
+ * if the number is 0: is zero
+ * if the number is less than 0: is negative
+ * */
 int main(void)
 {
-/*program to determine the polarity of a number*/
+
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
