357e179c3890b7ebecd89335bcc3b7c0ad8ad4d1
dominion-30 <abiolaayobamidele@gmail.com>
2022-03-11 12:27:48 -0800

    Add file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,22 +6,24 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
+/*program to determine the polarity of a number*/
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-if (n>0)
+if (n > 0)
 {
-printf("%d is positive\n",n);
+    printf("%d is positive\n", n);
 }
 
-else if (n<0)
+else if (n < 0)
 {
-printf("%d is negative\n",n);
+    printf("%d is negative\n", n);
 }
 
-else{
-printf("%d is zero\n",n);
+else
+{
+    printf("%d is zero\n", n);
 }
 
 /* your code goes there */
