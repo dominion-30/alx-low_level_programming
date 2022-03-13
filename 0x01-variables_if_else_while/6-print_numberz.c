3f6a657137bab5c643cea2e460c10b2b695d916d
dominion-30 <abiolaayobamidele@gmail.com>
2022-03-13 12:54:30 -0700

    Add file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void)
+{
+	/*this program returns the object of type int*/
+int i;/*i is counter variable*/
+for (i=0; i<10; i++)
+{
+
+
+	putchar(i);
+	putchar();
+}
+return(0);
+
+}
