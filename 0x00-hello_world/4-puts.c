59a57b65c0867f7743e7210c0d6f821c80a37911
dominion-30 <abiolaayobamidele@gmail.com>
2022-03-14 02:15:07 -0700

    Add file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void){
-char text;
-text =  "\"Programming is like building a multilingual puzzle";
-puts(text);
+/*This program display a text at stdout*/
+
+puts("\"Programming is like building a multilingual puzzle");
 return(0);
 }
