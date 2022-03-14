6e04e110287569cedcfb9c1608aae4264725d5ad
dominion-30 <abiolaayobamidele@gmail.com>
2022-03-14 01:01:29 -0700

    modify file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,11 +4,11 @@
 			 *          * This program returns the sizes of different data type
 			 *                   * 
 			 *                            * */
-		    printf("size of a char: %d byte\n", sizeof(char));
-		        printf("size of an int: %d byte\n", sizeof(int));
-			    printf("size of a long int: %d byte\n", sizeof(long int));
-			        printf("size of a long long int: %d byte\n", sizeof(long long int));
-				    printf("size of a float: %d byte\n", sizeof(float));
+		    printf("size of a char: %d byte(s)\n", sizeof(char));
+		        printf("size of an int: %d byte(s)\n", sizeof(int));
+			    printf("size of a long int: %d byte(s)\n", sizeof(long int));
+			        printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
+				    printf("size of a float: %d byte(s)\n", sizeof(float));
 				        return(0);
 	}
 	
