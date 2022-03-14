8d7542b3e489ebe566fdea64a892ba6617a5deba
dominion-30 <abiolaayobamidele@gmail.com>
2022-03-14 02:34:25 -0700

    Add files

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /*this program returns to standard error*/
 int main(void){
-fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 return(1);
 }
