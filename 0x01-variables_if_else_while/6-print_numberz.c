1ae2d6827679d5fc9f1a37797beca46398cd237a
dominion-30 <abiolaayobamidele@gmail.com>
2022-03-14 12:10:24 -0700

    modify file

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,12 +4,14 @@ int main(void)
 	/*this program returns the object of type int*/
 int i;/*i is counter variable*/
 i = 0;
+int c;
+c = getchar();
 for (i=0; i<10; i++)
 {
 
 
 	putchar(i);
-	putchar();
+	putchar(c);
 }
 return(0);
 
