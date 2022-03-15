ec7f594a7a076c8ce7cde11eac757f7210c63b44
dominion-30 <abiolaayobamidele@gmail.com>
2022-03-14 18:33:48 -0700

    Add files

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
+
+/**
+* main - all alphabet except e and q
+*
+**/
 int main(void)
 {
-char letter;
-for(letter = "a"; letter <="z"; letter++){
 
-	if (letter =="e" || letter =="q"){
+	char letter;
+	for(letter = 'a'; letter <='z'; letter++){
+
+		if (letter =='e' || letter =='q')
+		{
+
 	
-		continue;
-	}
-	else{
-	putchar(letter);
-	}
-}
-return(0);
+			continue;
+		}	
+		else
+		{
+			putchar(letter);
+		}	
+	}	
+	return(0);
 
 }
