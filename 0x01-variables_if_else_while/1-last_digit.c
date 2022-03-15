db612390be9781a791c7e335d62d0814fef93720
dominion-30 <abiolaayobamidele@gmail.com>
2022-03-14 17:42:00 -0700

    Add changes

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 
 	int n;
 	int last_digit;
-	(time(0));
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
 	if (last_digit > 5)
@@ -32,4 +32,4 @@ int main(void)
 		printf("The Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);  
 	return (0);
 
-}
+i}
