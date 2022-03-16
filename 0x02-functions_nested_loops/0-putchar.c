e1a69e92757c9c65febdaa02e221c2fe82afddb2
dominion-30 <abiolaayobamidele@gmail.com>
2022-03-16 01:49:17 -0700

    Add changes to file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* main - check the code for Holberton School students.
+*
+* Return: Always 0.
+*/
+
+int main()
+{
+	char first_character;
+	char second_character;
+	char third_character;
+	char fourth_character;
+	char fifth_character;
+	char sixth_character;
+	char seventh_character;
+	char eighth_character;
+
+	first_character = _putchar('_');
+	second_character = _putchar('p');
+	third_character =_putchar('u');
+	fourth_character = _putchar('t');
+	fifth_character = _putchar('c');
+	sixth_character = _putchar('h');
+	seventh_character = _putchar('a');
+	eighth_character = _putchar('r');
+        _putchar('\n');
+	return(0);
+}
