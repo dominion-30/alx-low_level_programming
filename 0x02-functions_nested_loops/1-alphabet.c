6f14881b0891c0401d358ebde67d3db3b30acdc4
dominion-30 <abiolaayobamidele@gmail.com>
2022-03-16 05:43:21 -0700

    Add changes to file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* main - prints lowercase letters of the alphabet to stdout
+*
+*
+* Return : Always 0 success
+*/
+
+int main(void)
+{
+	print_alphabet();
+	return(0);
+}
