0329fb482944371b9481e06a1225a4e73abb3c3a
dominion-30 <abiolaayobamidele@gmail.com>
2022-03-16 12:20:54 -0700

    Add changes to file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,16 @@
 */
 
 
-int main(void){
-	int _putchar('_');
+int main(void)
+{		
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
-        _putchar('t');
+	_putchar('t');
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n');    
-	return(0);	}
+	_putchar('\n');
+	return(0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
+
 /**
 * _putchar - writes the character c to stdout
 * @c: The character to print
@@ -5,10 +6,9 @@
 * Return: On success 1.
 * On error, -1 is returned, and errno is set appropriately.
 */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));     	
-}
+int _putchar(char c);
+
+
 	
 
 /**
@@ -17,4 +17,4 @@ int _putchar(char c)
 * Return: On success 1.
 */
 
-void print_alphabet(void)
+void print_alphabet(void);
