2065edc104fadcb6b29c445ead715e2a4e79fbc1
dominion-30 <abiolaayobamidele@gmail.com>
2022-03-17 11:37:17 -0700

    Add changes to file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,9 +8,16 @@ int _isupper(int c);
 
 
 /**
-* int _isdigit(int c)
+* int _isdigit(int c) checks for digit
 * Return 1 if c is a digit
-* Return 0 other
+* Return 0 otherwise
 */
 
 int _isdigit(int c);
+
+
+/**
+* int mul(int a, int b); function that multiplies two numbers
+*
+* Return the result
+*/
