2659718939b1aee2721928ec9a41c574fdef2a90
dominion-30 <abiolaayobamidele@gmail.com>
2022-03-17 17:03:46 -0700

    add fizz_buzz file

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* description: print  1 to 100 but for;
+* multiples of 3 print Fizz
+* multiples of 5 print Buzz
+* multiples of both print FizzBuzz
+*
+* Returns: Always 0 success
+*/
+
+int main()
+
+{
+	 int num;
+	 for (num =1; num <=100; num++)
+	 {
+		if (num % 3 == 0)
+
+		{	
+			printf(" Fizz ");
+
+		}
+
+		else if (num % 5 == 0)
+		{
+		
+			printf(" Buzz ");
+		}
+
+		else if (num % 3 == 0 && num % 5 == 0 )
+		{
+
+			printf(" FizzBuzz ");
+		}
+
+		else
+		{
+			printf(" %d ",num);
+		}
+
+
+	}
+	return (0);
+
+}
