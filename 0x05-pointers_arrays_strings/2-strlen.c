e4a91cf3acfcb5ac412128a9676bd024ea9263d2
dominion-30 <abiolaayobamidele@gmail.com>
2022-03-21 13:08:51 -0700

    add 2_strlen file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+* _strlen - function that returns the length of a string
+*
+* Return: Always int value of length of string
+*/
+int _strlen(char *s)
+
+{	
+	
+	int len;
+
+
+	len = strlen(s);
+	return (len);
+
+
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,10 @@
 /* prototype for function that takes a pointer to an int as parameter and updates the value it points to to 98*/
 void reset_to_98(int *n);
 
+
 /* prototype for function that swaps the value of two integers*/
 void swap_int(int *a, int *b);
+
+/* prototype for function that tells the length  of a string*/
+
+int _strlen(char *s);
