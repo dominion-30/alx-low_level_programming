cdb99e0c1171c4873ffd0a619b246a2736b159fb
dominion-30 <abiolaayobamidele@gmail.com>
2022-03-21 12:00:50 -0700

    add 1_swap file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,2 +1,5 @@
-/*function that takes a pointer to an int as parameter and updates the value it points to to 98*/
+/* prototype for function that takes a pointer to an int as parameter and updates the value it points to to 98*/
 void reset_to_98(int *n);
+
+/* prototype for function that swaps the value of two integers*/
+void swap_int(int *a, int *b);
