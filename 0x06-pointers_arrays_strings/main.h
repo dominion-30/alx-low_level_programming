1d8b3442187a5efec6741de3abbb095aefda5fe1
dominion-30 <abiolaayobamidele@gmail.com>
2022-03-24 17:32:18 +0100

    add 2-strncpy file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,3 +5,6 @@ char *_strcat(char *dest, char *src);
 /*prototype for function that concatenate two strings, except that it will use at most n bytes from src*/
 
 char *_strncat(char *dest, char *src, int n);
+
+/* prototype for a function that copies a string*/
+char *_strncpy(char *dest, char *src, int n);
