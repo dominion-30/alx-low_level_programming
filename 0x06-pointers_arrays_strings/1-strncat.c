5cb3ecd1b0fb06ac83b82db96417a3d66d9267b1
dominion-30 <abiolaayobamidele@gmail.com>
2022-03-24 14:07:58 +0100

    add 1-strncat file

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,9 +1,17 @@
 #include "main.h"
 #include <string.h>
 
+/**
+* _strncat - function that concatenate two strings
+* @str: char
+* Return dest
+*/
+
+
+
 char *_strncat(char *dest, char *src, int n)
 {
 	strcat(dest,src, n);
 	return dest;
-
+	
 }
