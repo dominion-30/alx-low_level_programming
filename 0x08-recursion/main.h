c5209b4f8ad2d08b373be513af4798109d9cc744
dominion-30 <abiolaayobamidele@gmail.com>
2022-03-30 22:38:14 +0100

    Add 6-is_prime_number file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -20,3 +20,7 @@ int _sqrt_recursion(int n);
 
 /* prototype of function that returns the value of x raised to the power of y */
 int _pow_recursion(int x, int y);
+
+/* prototype for function that returns 1 if the input integer is a prime number, otherwise returns 0*/
+
+int is_prime_number(int n);
