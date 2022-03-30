de4ec3a09a57235a254a9507dc1e534bb35659dd
dominion-30 <abiolaayobamidele@gmail.com>
2022-03-30 22:28:53 +0100

    Add changes to 1-print_rev_recursion file

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 #include<string.h>
+
+/**
+ * _print_rev_recursion - prints a string in reverse.
+ * @s: string
+ * Return: no return.
+ */
+
+
 void _print_rev_recursion(char *s)
 {
 	int len;
