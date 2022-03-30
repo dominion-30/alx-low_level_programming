2991752e78d5c90511fba075c8c1d4732e9cf7c4
dominion-30 <abiolaayobamidele@gmail.com>
2022-03-30 22:26:13 +0100

    Add changes to 3-factorial file

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * factorial - returns the factorial of a given number.
+ * @n: input number.
+ * Return: factorial of the number.
+ */
+
+
 int factorial(int n)
 {
 	if (n < 0)
