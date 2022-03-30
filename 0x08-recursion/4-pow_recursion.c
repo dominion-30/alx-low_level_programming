3005feddfb13e0cca0825060194c849a5514a699
dominion-30 <abiolaayobamidele@gmail.com>
2022-03-30 22:12:19 +0100

    Add 5-sqrt_recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <math.h>
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return -1;
+	}
+
+	return pow(x,y);
+
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,3 +13,10 @@ int _strlen_recursion(char *s);
 
 /* prototype for function that returns the factorial of a given number*/
 int factorial(int n);
+
+/* prototype for function that that returns the natural square root of a number*/
+
+int _sqrt_recursion(int n);
+
+/* prototype of function that returns the value of x raised to the power of y */
+int _pow_recursion(int x, int y);
